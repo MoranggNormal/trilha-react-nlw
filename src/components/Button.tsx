ac9cf1155d25a9828c6fa241598bbda69920e3a8
eullerpeixoto <eullerz01@hotmail.com>
2021-06-23 19:10:51 -0300

    Finalizada estilização básica, adicionado react-router-dom.

---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+import { ButtonHTMLAttributes } from "react";
+
+import '../styles/button.scss'
+
+type Buttonprops = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button (props: Buttonprops) {
+
+  return (
+    <button className="button" {...props} />
+  )
+}
