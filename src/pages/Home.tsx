357f41e88a9ecf6a11bbf0ae3fd7c2c774f5ef03
eullerpeixoto <eullerz01@hotmail.com>
2021-06-23 20:13:33 -0300

    Autenticação com Firebase.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,26 @@
 import { useHistory } from "react-router-dom";
+import { auth, firebase } from "../services/firebase";
 
 import IllustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
+
 import { Button } from '../components/Button';
+
+
 import '../styles/auth.scss';
 
 export default function Home () {
 
   const history = useHistory();
 
-  function navigateToNewRoom() {
+  function handleCreateROom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then(result => {
+      console.log(result)
+    })
+
     history.push('/rooms/new');
   }
 
@@ -26,7 +36,7 @@ export default function Home () {
     <div className="main-content">
       <img src={logoImg} alt="Letmeask" />
 
-      <button onClick={navigateToNewRoom} className="create-room">
+      <button onClick={handleCreateROom} className="create-room">
         <img src={googleIconImg} alt="Logo do Google" />
         Crie sua sala com o Google
       </button>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import '../styles/auth.scss';
 
 export default function NewRoom () {
+
   return (
     <div id="page-auth">
     <aside>
