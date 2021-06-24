a8328f7ef32d3173c92973127d388ee6989f83b5
eullerpeixoto <eullerz01@hotmail.com>
2021-06-23 22:59:52 -0300

    Autenticação e recuperação de autenticação finalizada.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,61 +1,55 @@
+import { useContext } from "react";
 import { useHistory } from "react-router-dom";
-import { auth, firebase } from "../services/firebase";
+import { AuthContext } from "../App";
+import IllustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
 
-import IllustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
+import { Button } from "../components/Button";
 
-import { Button } from '../components/Button';
+import "../styles/auth.scss";
 
-
-import '../styles/auth.scss';
-
-export default function Home () {
-
+export default function Home() {
   const history = useHistory();
+  const {user, signInWithGoogle} = useContext(AuthContext)
 
-  function handleCreateROom() {
-    const provider = new firebase.auth.GoogleAuthProvider();
+  async function handleCreateROom() {
+    if(!user){
+      await signInWithGoogle()
+    }
 
-    auth.signInWithPopup(provider).then(result => {
-      console.log(result)
-    })
-
-    history.push('/rooms/new');
+    history.push("/rooms/new");
   }
 
   return (
     <div id="page-auth">
-    <aside>
-      <img src={IllustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-      <strong>Crie salas de Q&amp;A ao-vivo </strong>
-      <p>Tire as dúvidas da sua audiência em tempo real</p>
-    </aside>
+      <aside>
+        <img
+          src={IllustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas de Q&amp;A ao-vivo </strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
 
-    <main>
-    <div className="main-content">
-      <img src={logoImg} alt="Letmeask" />
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          {user ? <h3>Olá, {user.name}.</h3> :
+          <button onClick={handleCreateROom} className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          }
 
-      <button onClick={handleCreateROom} className="create-room">
-        <img src={googleIconImg} alt="Logo do Google" />
-        Crie sua sala com o Google
-      </button>
+          <div className="separator">ou entre em uma sala</div>
 
-      <div className="separator">
-        ou entre em uma sala
-      </div>
-
-      <form>
-      <input
-      type="text"
-      placeholder="Digite o código da sala"
-      />
-      <Button type="submit">
-        Entrar na sala
-      </Button>
-      </form>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+        </div>
+      </main>
     </div>
-    </main>
-    </div>
-  )
+  );
 }
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,35 +1,44 @@
-import IllustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import { Button } from '../components/Button';
-import { Link } from 'react-router-dom';
-import '../styles/auth.scss';
+import { useContext } from "react";
+import { AuthContext } from "../App";
 
-export default function NewRoom () {
+
+import IllustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import { Button } from "../components/Button";
+import { Link } from "react-router-dom";
+import "../styles/auth.scss";
+
+export default function NewRoom() {
+
+  const {user, signInWithGoogle} = useContext(AuthContext)
+
 
   return (
     <div id="page-auth">
-    <aside>
-      <img src={IllustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-      <strong>Crie salas de Q&amp;A ao-vivo </strong>
-      <p>Tire as dúvidas da sua audiência em tempo real</p>
-    </aside>
+      <aside>
+        <img
+          src={IllustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas de Q&amp;A ao-vivo </strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
 
-    <main>
-    <div className="main-content">
-      <img src={logoImg} alt="Letmeask" />
-        <h2>Criar uma nova sala</h2>
-      <form>
-      <input
-      type="text"
-      placeholder="Digite o nome da sala"
-      />
-      <Button type="submit">
-        Criar na sala
-      </Button>
-      </form>
-      <p> Quer entrar em uma sala existente? <Link to="/">clique aqui</Link></p>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          {user && <h1>Bem vindo de volta, {user?.name}</h1>}
+          <h2>Criar uma nova sala</h2>
+          <form>
+            <input type="text" placeholder="Digite o nome da sala" />
+            <Button type="submit">Criar na sala</Button>
+          </form>
+          <p>
+            {" "}
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+          </p>
+        </div>
+      </main>
     </div>
-    </main>
-    </div>
-  )
+  );
 }
